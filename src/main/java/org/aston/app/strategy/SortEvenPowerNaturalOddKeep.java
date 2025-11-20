a0dcc024de8caabaacca9926f52212dca5b53f02
dmitry-chernikov <chernicov_dima@mail.ru>
2025-11-20 15:38:16 +0400

    Реализация сортировок.

--- src/main/java/org/aston/app/strategy/SortEvenPowerNaturalOddKeep.java
+++ src/main/java/org/aston/app/strategy/SortEvenPowerNaturalOddKeep.java
@@ -9,9 +9,53 @@ package org.aston.app.strategy;
  * Class SortEvenPowerNaturalOddKeep
  */
 
+import org.aston.app.model.Car;
+
 /**
  * Дополнительное задание 1: сортировка по полю power.
  * Четные значения сортируются по возрастанию, нечетные остаются на своих местах.
  */
 public class SortEvenPowerNaturalOddKeep implements SortStrategy {
+    @Override
+    public void sort(Car[] cars) {
+        // Извлекаем четные значения мощности
+        int[] evenPowers = new int[cars.length];
+        int evenCount = 0;
+
+        for (Car car : cars) {
+            if (car.getPower() % 2 == 0) {
+                evenPowers[evenCount++] = car.getPower();
+            }
+        }
+
+        // Сортируем четные значения
+        for (int i = 0; i < evenCount - 1; i++) {
+            for (int j = 0; j < evenCount - 1 - i; j++) {
+                if (evenPowers[j] > evenPowers[j + 1]) {
+                    int temp = evenPowers[j];
+                    evenPowers[j] = evenPowers[j + 1];
+                    evenPowers[j + 1] = temp;
+                }
+            }
+        }
+
+        // Возвращаем отсортированные четные значения обратно в массив
+        int evenIndex = 0;
+        for (int i = 0; i < cars.length; i++) {
+            if (cars[i].getPower() % 2 == 0) {
+                int originalPower = cars[i].getPower();
+                // Находим отсортированное значение
+                int newPower = evenPowers[evenIndex++];
+                if (newPower != originalPower) {
+                    // Пересоздаем объект через Builder (неизменяемость)
+                    Car newCar = new Car.Builder()
+                            .setModel(cars[i].getModel())
+                            .setPower(newPower)
+                            .setYear(cars[i].getYear())
+                            .build();
+                    cars[i] = newCar;
+                }
+            }
+        }
+    }
 }
