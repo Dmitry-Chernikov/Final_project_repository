163a38673eeadc1be851d58f2e40dcd53ecff004
feofanovivan-aston <feofanovivan2012@gmail.com>
2025-11-26 18:48:43 +0300

    Main.java

--- src/org/aston/app/Main.java
+++ src/org/aston/app/Main.java
@@ -1,6 +1,7 @@
 package org.aston.app;
 
 import org.aston.app.service.SortingApplication;
+import org.aston.app.service.FileService;
 
 public class Main {
     public static void main(String[] args) {
